e946392dc363ab6ed691e2dccfe9bc5899c539aa
JayeZhu <1032657951@qq.com>
2019-07-27 01:02:34 +0800

    增加CRUD

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,33 +1,43 @@
 module.exports = app => {
   const express = require('express')
-  const router = express.Router()
-  const Category = require('../../models/Category')
+  const router = express.Router({
+    mergeParams: true
+  })
+  
   // 增
-  router.post('/categories', async (req, res) => {
-    const model = await Category.create(req.body)
+  router.post('/', async (req, res) => {
+    const model = await req.Model.create(req.body)
     res.send(model)
   });
   // 查 populate - 查看父级
-  router.get('/categories', async (req, res) => {
-    const items = await Category.find().populate('parent').limit(10);
+  router.get('/', async (req, res) => {
+    const queryOptions = {};
+    if (req.Model.modelName === 'Category') {
+      queryOptions.populate = 'parent'
+    }
+    const items = await req.Model.find().setOptions(queryOptions).limit(10);
     res.send(items);
   });
   // 根据ID查
-  router.get('/categories/:id', async (req, res) => {
-    const model = await Category.findById(req.params.id);
+  router.get('/:id', async (req, res) => {
+    const model = await req.Model.findById(req.params.id);
     res.send(model);
   });
   // 改
-  router.put('/categories/:id', async (req, res) => {
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body);
+  router.put('/:id', async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
     res.send(model);
   });
   // 删
-  router.delete('/categories/:id', async (req, res) => {
-    const model = await Category.findByIdAndDelete(req.params.id, req.body);
+  router.delete('/:id', async (req, res) => {
+    const model = await req.Model.findByIdAndDelete(req.params.id, req.body);
     res.send({
       success: true
     });
   });
-  app.use('/admin/api', router);
+  app.use('/admin/api/rest/:resource', async (req, res, next) => {
+    const modelName = require('inflection').classify(req.params.resource);
+    req.Model = require(`../../models/${modelName}`);
+    next()
+  }, router);
 }
