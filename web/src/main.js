55bd81e0d1549673a31af7ff7ed132f68e4289f6
JayeZhu <1032657951@qq.com>
2019-08-14 01:05:47 +0800

    轮播图

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
-import './style.scss'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+ 
+// require styles
+import 'swiper/dist/css/swiper.css'
+ 
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
+import './assets/scss/style.scss'
 import router from './router'
 
 new Vue({
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="home">
-    首页
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="">
+    </swiper-slide>
+    <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+  </swiper>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    swiperOption: {
+      pagination: {
+        el: '.pagination-home'
+      }
+    },
+  })
 }
 </script>
+
+<style lang="scss">
+@import '../assets/scss/variables';
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white")!important;
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info")!important;
+    }
+  }
+}
+</style>
+
